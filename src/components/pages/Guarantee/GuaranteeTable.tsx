379395c398ac61986953faa98fbf3e5dddfc44bf
phucluu1609 <phuc.luu@jrgvn.com>
2023-05-01 10:47:59 +0700

    chore: update table guarantee UI

---
+++ src/components/pages/Guarantee/GuaranteeTable.tsx
@@ -0,0 +1,56 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material"
+import { StyledTableCell, StyledTableRow } from "components/common/StyledTable"
+import { storeApi } from "components/data/storeApi"
+
+const TableDetail = () => {
+  const handleClick = (value: string) => {
+    window.open(`https://maps.google.com/maps?q=${value}`, "_blank")
+  }
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }}>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">Num</StyledTableCell>
+              <StyledTableCell align="center">Địa chỉ</StyledTableCell>
+              <StyledTableCell align="center">Điện thoại</StyledTableCell>
+              <StyledTableCell align="center">Thời gian</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {storeApi.map((row, index) => (
+              <StyledTableRow key={index}>
+                <StyledTableCell align="center">{index + 1}</StyledTableCell>
+                <StyledTableCell
+                  align="center"
+                  onClick={() => handleClick(row[0])}
+                  sx={{
+                    "&:hover": {
+                      cursor: "pointer",
+                      color: (theme) => theme.palette.secondary.main,
+                    },
+                  }}
+                >
+                  {row[0]}
+                </StyledTableCell>
+                <StyledTableCell align="center">{row[1]}</StyledTableCell>
+                <StyledTableCell align="center">{row[2]}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  )
+}
+
+export default TableDetail
--- src/components/pages/Guarantee/index.tsx
+++ src/components/pages/Guarantee/index.tsx
@@ -1,13 +1,52 @@
+import { Box, Grid, Typography } from "@mui/material"
+import { animated, useSpring } from "@react-spring/web"
 import { Helmet } from "react-helmet"
+import TableDetail from "./GuaranteeTable"
 
 const GuaranteePage = () => {
+  const springProps = useSpring({
+    from: { transform: "translateX(100%)" },
+    to: { transform: "translateX(-100%)" },
+    delay: 100,
+    config: { duration: 15000 },
+    loop: true,
+  })
+
   return (
     <>
       <Helmet>
-        <title>Phone Pro | Guarantee</title>
+        <title>Phone Pro | Trung tâm bảo hành</title>
         <meta name="description" content="Description of Guarantee Page ..." />
       </Helmet>
-      Guarantee Page
+
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Box
+            p={3}
+            sx={{
+              bgcolor: (theme) => theme.palette.primary.dark,
+            }}
+            overflow="hidden"
+            display={{ xs: "none", md: "block" }}
+          >
+            <animated.div style={springProps}>
+              <Typography
+                variant="h5"
+                color="white"
+                component="h1"
+                textTransform="uppercase"
+                letterSpacing={1}
+                fontWeight={600}
+              >
+                Danh sách các trung tâm bảo hành tại Phone Pro
+              </Typography>
+            </animated.div>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <TableDetail />
+        </Grid>
+      </Grid>
     </>
   )
 }
