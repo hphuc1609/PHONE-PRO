03eb8dee2cb20bf012d77c40c27d3a6294cbdf68
phucluu1609 <phuc.luu@jrgvn.com>
2023-05-07 00:08:04 +0700

    chore: initial utils config

---
+++ src/utils/specification.ts
@@ -0,0 +1,40 @@
+import { ISpecification } from "models/specification"
+
+export const specifications: ISpecification[] = [
+  {
+    label: "Màn hình",
+    value: "Màn hình",
+  },
+  {
+    label: "Hệ điều hành",
+    value: "Hệ điều hành",
+  },
+  {
+    label: "Camara sau",
+    value: "Camara sau",
+  },
+  {
+    label: "Camara trước",
+    value: "Camara trước",
+  },
+  {
+    label: "CPU",
+    value: "CPU",
+  },
+  {
+    label: "RAM",
+    value: "RAM",
+  },
+  {
+    label: "Bộ nhớ trong",
+    value: "Bộ nhớ trong",
+  },
+  {
+    label: "Thẻ nhớ",
+    value: "Thẻ nhớ",
+  },
+  {
+    label: "Dung lượng pin",
+    value: "Dung lượng pin",
+  },
+]
