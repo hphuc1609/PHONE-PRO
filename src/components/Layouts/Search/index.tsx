cc22f694231a6ab54278dafa8cfa8108ed093a2b
phucluu1609 <phuc.luu@jrgvn.com>
2023-05-07 00:06:02 +0700

    chore: update UI and handle word key search logic

--- src/components/Layouts/Search/index.tsx
+++ src/components/Layouts/Search/index.tsx
@@ -1,93 +1,130 @@
 import { Search } from "@mui/icons-material"
 import {
-  Box,
-  Grid,
-  IconButton,
-  InputAdornment,
-  TextField,
+  Autocomplete,
+  Divider,
+  InputBase,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Tooltip,
   Typography,
 } from "@mui/material"
-import { Link } from "react-router-dom"
+import { makeStyles } from "@mui/styles"
+import { ICustomAPIResponse } from "models/product"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { borderColor } from "styles/config"
+import { realtimeDB } from "utils/firebaseConfig"
 
-interface Props {
-  setSearchQuery: (searchQuery: string) => void
-}
+const useStyles = makeStyles(() => ({
+  input: {
+    height: 40,
+    width: 400,
+    padding: 20,
+    paddingRight: 10,
+    borderRadius: 100,
+    backgroundColor: "#fff",
+    lineHeight: 38,
+    fontSize: 13,
 
-const SearchSuggest = ({ setSearchQuery }: Props) => {
-  // const keyWords = ["Samsung", "Iphone", "Huawei", "Oppo"]
+    "&.Mui-focused": {
+      border: "1.5px solid #0C9",
+    },
+  },
+}))
 
-  const handleSearchSuggest = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value)
+const SearchSuggestion = () => {
+  const classes = useStyles()
+  const navigate = useNavigate()
+  const [productList, setProductList] = useState([])
+
+  useEffect(() => {
+    realtimeDB.ref("products").once("value", (snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val()
+        setProductList(data)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if ((e.ctrlKey || e.metaKey) && e.keyCode === 70) {
+        const searchInput = document.getElementById("search")
+        if (searchInput !== document.activeElement) {
+          e.preventDefault()
+          searchInput.focus()
+        } else {
+          return true
+        }
+      }
+      return undefined
+    })
+  }, [])
+
+  const handleOnChange = (value: ICustomAPIResponse) => {
+    navigate(`/detail/${value.productId}`)
   }
 
   return (
-    <Box flexGrow={1} textAlign="center">
-      <TextField
-        id="search-bar"
-        onChange={handleSearchSuggest}
-        placeholder="Tìm kiếm sản phẩm..."
-        variant="outlined"
-        size="small"
-        sx={{
-          width: "50%",
-          bgcolor: "white",
-          borderRadius: "5px",
-
-          "& .MuiOutlinedInput-root": {
-            padding: "0 5px",
-            outline: "none",
-            "& fieldset": {
-              borderColor: "transparent",
-              border: "none",
-            },
-          },
-        }}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton>
-                <Search />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-
-      {/* <Grid
-        container
-        display={{ xs: "none", md: "flex" }}
-        width="50%"
-        margin="8px auto 0"
-      >
-        <Grid item xs={12} display="flex">
-          <Typography mr={1}>Từ khóa:</Typography>
-          {keyWords.map((key, index) => (
-            <Grid
-              item
-              xs={0}
-              key={index}
-              sx={{
-                "&:hover": {
-                  textDecoration: "underline",
-                },
-              }}
-            >
-              <Link
-                to=""
-                style={{
-                  color: "inherit",
-                  textDecoration: "none",
-                  marginRight: 10,
-                }}
-              >
-                {key}
-              </Link>
-            </Grid>
-          ))}
-        </Grid>
-      </Grid> */}
-    </Box>
+    <Autocomplete
+      id="search"
+      freeSolo
+      selectOnFocus
+      autoHighlight
+      loading={true}
+      loadingText="Không tìm thấy sản phẩm"
+      options={productList}
+      renderOption={(props, option: ICustomAPIResponse) => (
+        <>
+          <ListItem
+            component="span"
+            dense
+            {...props}
+            sx={{
+              columnGap: 1,
+              borderBottom: `1px solid #e8e8e8`,
+              "&:last-child": { border: "none" },
+            }}
+          >
+            <ListItemIcon sx={{ width: 50, height: 80, py: 1 }}>
+              <img
+                src={option.photoImage}
+                alt="..."
+                width="100%"
+                height="100%"
+                style={{ objectFit: "cover" }}
+              />
+            </ListItemIcon>
+            <ListItemText primary={option.title} sx={{ cursor: "pointer" }} />
+            <Typography variant="body2" color="error" fontWeight={500}>
+              {option.price}
+              <span style={{ fontSize: 10, position: "relative", top: -5 }}>
+                ₫
+              </span>
+            </Typography>
+          </ListItem>
+        </>
+      )}
+      getOptionLabel={(options) => options.productName || ""}
+      onChange={(e, value) => value !== undefined && handleOnChange(value)}
+      renderInput={(params) => {
+        return (
+          <InputBase
+            {...params}
+            {...params.InputProps}
+            placeholder="Bạn tìm gì..."
+            className={classes.input}
+            endAdornment={
+              <Tooltip arrow title="Ctrl+F" placement="right">
+                <Search sx={{ cursor: "default" }} />
+              </Tooltip>
+            }
+          />
+        )
+      }}
+    />
   )
 }
 
-export default SearchSuggest
+export default SearchSuggestion
