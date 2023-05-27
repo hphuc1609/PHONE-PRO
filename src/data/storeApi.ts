e54626e4345826f3e90aae9b165138f2ec14b536
Phuc Luu <phuc.luu@jrgvn.com>
2023-05-27 20:50:52 +0700

    chore: config api JSON

---
+++ src/data/storeApi.ts
@@ -0,0 +1,69 @@
+export const storeApi = [
+  [
+    "10F2, Hồ Trung Thành, P7 – Tp. Cà Mau, Tỉnh Cà Mau",
+    "(0780) 2212 158",
+    "8:00 - 17:00",
+  ],
+  [
+    "14A2 Trần Nguyên Hãn, P. Mỹ Long, Long Xuyên, An Giang",
+    "076 3841 649",
+    "8:00 - 17:00",
+  ],
+  [
+    "114 Tô Hiệu, Quận Lê Chân, Tp. Hải Phòng",
+    "(031) 384 7689",
+    "8:00 - 17:00",
+  ],
+  ["32 Lương Khánh Thiện, Tp. Hải phòng", "092 4713 257", "8:00 - 17:00"],
+  [
+    "123 Nam Kỳ Khởi Nghĩa, Tp. Vũng Tàu, Tỉnh BRVT",
+    "(064) 3531 248",
+    "8:00 - 17:00",
+  ],
+  [
+    "157 Ngô Gia Tự, Phường Ngô Quyền, Tp. Bắc Giang",
+    "(0240) 3820 349",
+    "8:00 - 17:00",
+  ],
+  ["32 Lương Khánh Thiện, Tp. Hải phòng", "(0781) 3827 676", "8:00 - 17:00"],
+  [
+    "139 Nguyễn Văn Cừ, Tp. Bắc Ninh, Tỉnh Bắc Ninh",
+    "(0241) 3812 767",
+    "8:00 - 17:00",
+  ],
+  [
+    "39 Nguyễn Đình Chiểu, P1, Tx. Bến Tre, Tỉnh Bến Tre",
+    "(075) 3814 701",
+    "8:00 - 17:00",
+  ],
+  [
+    "10A, Lý Thường Kiệt, Tp. Quy Nhơn, Tỉnh Bình Định",
+    "(056) 3821 788",
+    "8:00 - 17:00",
+  ],
+  [
+    "42 Phố 11 Vân Giang, P. Vân Giang, Tp. Ninh Bình",
+    "(030) 389 3408",
+    "8:00 - 17:00",
+  ],
+  [
+    "283 Cách Mạng Tháng Tám, Tx.Thủ Dầu Một, Tỉnh Bình Dương",
+    "(0650) 3831 528",
+    "8:00 - 17:00",
+  ],
+  [
+    "47 Khu 2, P. Phước Bình, Tx. Phước Long, Bình Phước",
+    "(0651) 3774 789",
+    "8:00 - 17:00",
+  ],
+  [
+    "20 Nguyễn Hội P.Phú Trinh Tp.Phan Thiết, Tỉnh Bình Thuận",
+    "062 382 853",
+    "8:00 - 17:00",
+  ],
+  [
+    "76 Nguyễn Đình Chiểu, P2, Tp. Cao Lãnh, Đồng Tháp",
+    "(067) 3874 686",
+    "8:00 - 17:00",
+  ],
+]
