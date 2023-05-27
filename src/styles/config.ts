c050c80595145a86a3b99e93609d5301f0223f10
Phuc Luu <phuc.luu@jrgvn.com>
2023-05-27 20:46:11 +0700

    chore: update config styles

--- src/styles/config.ts
+++ src/styles/config.ts
@@ -1,7 +1,7 @@
 import theme from "./theme"
 
-export const mainBackground = "#f9fafc"
+export const mainBackground = "#fff"
 export const primaryDark = theme.palette.primary.dark
-export const appBarHeight = "5rem"
+export const headerHeight = "5rem"
 export const borderColor = "#cccc"
 export const zIndex = theme.zIndex.modal
