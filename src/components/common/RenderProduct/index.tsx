f5dcdbad64679d778478ecd0bc7cc528697cb312
PL <luuboy91@gmail.com>
2023-06-28 08:18:18 +0700

    chore: update common RenderProduct

--- src/components/common/RenderProduct/index.tsx
+++ src/components/common/RenderProduct/index.tsx
@@ -1,36 +1,26 @@
 import { Grid, Typography } from "@mui/material"
-import { makeStyles } from "@mui/styles"
 import ProductListRow from "components/common/ProductList/ProductListRow"
 import { swiperConfig } from "configs/swiper"
 import { ICustomAPIResponse } from "models/product"
-import { primaryDark } from "styles/config"
 import { Swiper, SwiperSlide } from "swiper/react"
-
-const useStyles = makeStyles(() => ({
-  title: {
-    textTransform: "capitalize",
-    padding: 8,
-    borderRadius: 100,
-    textAlign: "center",
-    color: "white",
-    backgroundImage: `linear-gradient(to right, #3977ce, ${primaryDark})`,
-    transform: "translateY(-1.05em)",
-  },
-}))
 
 interface ProductBrandProps {
   data: ICustomAPIResponse[]
   title: string
   row: number
+  className?: ClassNameProps
 }
 
-const RenderProduct = ({ data, title, row }: ProductBrandProps) => {
-  const classes = useStyles()
+interface ClassNameProps {
+  root?: string
+  title?: string
+}
 
+const RenderProduct = ({ data, title, row, className }: ProductBrandProps) => {
   return (
-    <Grid container mt={8} border="2px solid #3977ce">
+    <Grid container className={className.root}>
       <Grid item xs={12}>
-        <Typography className={classes.title} variant="h5">
+        <Typography className={className.title} variant="h5">
           {title}
         </Typography>
       </Grid>
