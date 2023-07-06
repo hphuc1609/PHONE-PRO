d1342fa86418b241da68cbcc516ea07ecba79312
PL <luuboy91@gmail.com>
2023-07-06 23:44:51 +0700

    chore: add comment feature

---
+++ src/components/CommentBox/Comment.tsx
@@ -0,0 +1,140 @@
+import { Avatar, Box, Button, Typography } from "@mui/material"
+import InputTextEmoji from "components/common/InputTextEmoji"
+import { IComment } from "models/product"
+import React from "react"
+import { FieldValues, useForm } from "react-hook-form"
+import { primaryDark } from "styles/config"
+
+interface Props {
+  openReply: boolean | number
+  replyRef?: React.MutableRefObject<HTMLButtonElement>
+  comment: IComment
+  replies?: IComment[]
+  autoFocus?: boolean
+
+  handleDeleteComment: (id: number) => void
+  handleAddComment: (data: FieldValues) => void
+  handleOpenReply: (id: number) => void
+}
+
+const Comment = ({
+  openReply,
+  replyRef,
+  comment,
+  replies,
+  autoFocus,
+  handleDeleteComment,
+  handleAddComment,
+  handleOpenReply,
+}: Props) => {
+  const createDate = new Date(comment.createDate).toLocaleString().split(" ")[1]
+  const isReplyOpen = openReply === comment.id
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm()
+
+  const watchContent = watch("content")
+
+  const handleReset = () => {
+    reset({ content: "" })
+  }
+
+  return (
+    <Box mt={2} display="flex" gap={2}>
+      <Avatar sx={{ bgcolor: primaryDark }}>
+        {comment.username?.slice(0, 1)}
+      </Avatar>
+      <Box width="100%" display="flex" flexDirection="column">
+        <Box display="flex" alignItems="center" mb={1}>
+          <Typography variant="body1" fontWeight={500}>
+            {comment.username?.split("@")[0]}
+          </Typography>
+          <Typography variant="body2" color="GrayText" ml={1}>
+            {createDate}
+          </Typography>
+        </Box>
+        <Typography variant="body2">{comment.content}</Typography>
+        <Box>
+          {replies && (
+            <Button
+              variant="text"
+              color="primary"
+              size="small"
+              disableRipple
+              sx={{
+                padding: 0,
+                marginTop: 0.5,
+                marginLeft: -1.5,
+                textTransform: "none",
+              }}
+              onClick={() => handleOpenReply(comment.id)}
+            >
+              Trả lời
+            </Button>
+          )}
+          <Button
+            variant="text"
+            color="primary"
+            size="small"
+            disableRipple
+            sx={{
+              padding: 0,
+              marginTop: 0.5,
+              marginLeft: -1.5,
+              textTransform: "none",
+            }}
+            onClick={() => handleDeleteComment(comment.id)}
+          >
+            Xoá
+          </Button>
+        </Box>
+
+        {replies?.map((reply) => (
+          <Comment
+            key={reply.id}
+            comment={reply}
+            handleDeleteComment={() => handleDeleteComment(reply.id)}
+            handleAddComment={handleAddComment}
+            handleOpenReply={() => handleOpenReply(reply.id)}
+            openReply={openReply}
+            replyRef={replyRef}
+          />
+        ))}
+
+        {isReplyOpen && (
+          <>
+            <InputTextEmoji
+              control={control}
+              errors={errors}
+              reset={reset}
+              watchContent={watchContent}
+              autoFocus={autoFocus}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              sx={{ mt: 2, px: 4, alignSelf: "flex-end" }}
+              disabled={!watchContent}
+              onClick={handleSubmit(handleAddComment)}
+            >
+              Gửi
+            </Button>
+            <Button
+              ref={replyRef}
+              sx={{ display: "none" }}
+              onClick={handleReset}
+            />
+          </>
+        )}
+      </Box>
+    </Box>
+  )
+}
+
+export default Comment
---
+++ src/components/CommentBox/index.tsx
@@ -0,0 +1,186 @@
+import { Box, Button, Grid, Typography } from "@mui/material"
+import { createComponentWithAuth, firestoreDB } from "Firebase/firebaseConfig"
+import AlertDialog from "components/common/Dialog"
+import InputTextEmoji from "components/common/InputTextEmoji"
+import { IComment } from "models/product"
+import { useEffect, useRef, useState } from "react"
+import { FieldValues, useForm } from "react-hook-form"
+import { useNavigate, useParams } from "react-router-dom"
+import { WrappedComponentProps } from "react-with-firebase-auth"
+import { borderColor } from "styles/config"
+import Comment from "./Comment"
+
+const CommentBox = ({ user }: WrappedComponentProps) => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const replyRef = useRef(null)
+
+  const [dataComment, setDataComment] = useState([])
+  const [open, setOpen] = useState(false)
+  const [openReply, setOpenReply] = useState<number | boolean>()
+  const [autoFocus, setAutoFocus] = useState(false)
+
+  useEffect(() => {
+    const getDataComment = firestoreDB
+      .collection("comments")
+      .onSnapshot((res) => {
+        const data = res.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        setDataComment(data)
+      })
+
+    return () => getDataComment()
+  }, [])
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm()
+
+  const watchContent = watch("content")
+
+  const handleAddComment = (data: FieldValues) => {
+    const { content } = data
+    const createDate = new Date().toISOString()
+
+    if (user) {
+      const newComment: IComment = {
+        id: dataComment.length + 1,
+        content,
+        createDate,
+        userId: (dataComment.length + 1).toString(),
+        username: user.displayName || user.email.split("@")[0],
+        parentId: openReply ? openReply.toString() : null,
+        productId: id,
+      }
+      firestoreDB.collection("comments").add(newComment)
+      reset({ content: "" })
+
+      if (openReply) {
+        replyRef.current.click(), setOpenReply(false)
+      }
+    } else setOpen(true)
+  }
+
+  const getReplies = (commentId: number) => {
+    return dataComment
+      .filter((comment) => comment.parentId === commentId.toString())
+      .sort(
+        (a, b) =>
+          new Date(a.createDate).getTime() - new Date(b.createDate).getTime()
+      )
+  }
+
+  const filterdComments = dataComment.filter(
+    (comment) => comment.productId === id
+  )
+
+  const handleDeleteComment = (id: number) => {
+    firestoreDB
+      .collection("comments")
+      .get()
+      .then((res) => {
+        res.forEach((doc) => {
+          console.log(doc.data().id)
+          if (doc.data().id === id) {
+            doc.ref.delete()
+          }
+        })
+      })
+  }
+
+  const handleClose = () => setOpen(false)
+
+  const handleAccept = () => {
+    navigate("/login")
+    window.scrollTo(0, 0)
+  }
+
+  const handleOpenReply = (commentId: any) => {
+    if (openReply === commentId) {
+      setOpenReply(false)
+    } else {
+      setAutoFocus(true)
+      setOpenReply(commentId)
+    }
+  }
+
+  return (
+    <>
+      <Box
+        maxWidth={900}
+        border={`1px solid ${borderColor}`}
+        borderRadius={3}
+        p={3}
+        mt={10}
+      >
+        <Grid container spacing={5}>
+          <Grid item xs={12}>
+            <Typography variant="h5" fontWeight={500} gutterBottom>
+              Bình luận
+            </Typography>
+            <form
+              onSubmit={handleSubmit(handleAddComment)}
+              autoComplete="off"
+              noValidate
+            >
+              <InputTextEmoji
+                control={control}
+                errors={errors}
+                reset={reset}
+                watchContent={watchContent}
+              />
+
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                sx={{ mt: 2, px: 4 }}
+                disabled={!watchContent}
+              >
+                Gửi
+              </Button>
+            </form>
+          </Grid>
+
+          <Grid item xs={12}>
+            {filterdComments?.map(
+              (comment: IComment) =>
+                comment.parentId === null && (
+                  <Comment
+                    key={comment.id}
+                    openReply={openReply}
+                    replyRef={replyRef}
+                    comment={comment}
+                    replies={getReplies(comment.id)}
+                    autoFocus={autoFocus}
+                    handleDeleteComment={handleDeleteComment}
+                    handleAddComment={handleAddComment}
+                    handleOpenReply={handleOpenReply}
+                  />
+                )
+            )}
+          </Grid>
+        </Grid>
+      </Box>
+
+      <AlertDialog
+        open={open}
+        handleClose={handleClose}
+        fullWidthButton
+        content="Vui lòng đăng nhập tài khoản để bình luận !"
+        rightButton="Đăng nhập"
+        onClickRightButton={handleAccept}
+        leftButton="Đóng"
+        onClickLeftButton={handleClose}
+      />
+    </>
+  )
+}
+
+export default createComponentWithAuth(CommentBox)
